329bd7fbe0079408a52c421661cc13f191d68fa6
Victor <Victor.Gamble@outlook.com>
2020-03-10 15:23:04 -0400

    resolving the merge issue.

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
