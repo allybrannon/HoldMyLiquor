feee1f877528145a1d1e4a0f77477b33e5987e4c
Victor <Victor.Gamble@outlook.com>
2020-03-11 15:19:04 -0400

    added a get and post routes for the search page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express"),
   router = express.Router(),
   drinkModel = require("../models/drinkModel");
 
+
 /* GET home page. */
 router.get("/", async (req, res) => {
   const drinkData = await drinkModel.getOneCocktail();
@@ -16,5 +17,42 @@ router.get("/", async (req, res) => {
     }
   });
 });
+
+
+
+router.get("/search/:cocktailName?", async (req, res) => {
+  const {
+    cocktailName
+  } = req.params;
+  const drinkData = await drinkModel.searchCocktails(cocktailName);
+  console.log('DRINK DATA =', drinkData)
+
+  res.render("template", {
+    locals: {
+      title: "Results Page",
+      drinkData: drinkData
+    },
+    partials: {
+      partial: "partial-result"
+    }
+  });
+});
+
+
+router.post('/search', async (req, res) => {
+  const {
+    cocktailName
+  } = req.body;
+  console.log('POST DATA =', cocktailName)
+  const url = `/search/${cocktailName}`;
+  if (!!cocktailName) {
+    res.redirect(url);
+  } else {
+    res.redirect('/');
+  }
+
+})
+
+
 
 module.exports = router;
